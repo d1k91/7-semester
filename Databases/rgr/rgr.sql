23f3b12c31c175c62fec2e0d7863b296992d28fc
Vadim Glinskiy <v.glinskiy@icloud.com>
2025-10-20 00:03:56 +0700

    DB RGR 2. Запросы с использованием JOIN

--- Databases/rgr/rgr.sql
+++ Databases/rgr/rgr.sql
@@ -17,3 +17,34 @@ join responces r on js.seeker_id = r.seeker_id
 join vacancies v on r.vacancy_id = v.vacancy_id
 group by js.name
 having count(r.responce_id) >= 2;
+
+-- вывести информацию о будущих собеседованиях, а так же информацию о потенциальном работнике, вакансии и компании
+
+select 
+	js.name,
+	v.vacancy_name,
+	e.company_name,
+	i.interview_date,
+	i.result
+from interviews i 
+join responces r on i.responce_id = r.responce_id 
+join job_seekers js on r.seeker_id = js.seeker_id 
+join vacancies v on r.vacancy_id = v.vacancy_id 
+join employers e on v.employer_id = e.employer_id 
+where i.interview_date > current_timestamp;
+
+
+-- найти компании, у которых были успешные собеседования, и вывести информацию о соискателях
+
+select
+	e.company_name as "Компании",
+	count(distinct i.interview_id) as "Успешных собеседований",
+	count(distinct js.seeker_id ) as "Уникальных соискателей",
+	round(avg((v.salary_min + v.salary_max )/2), 2) as "Средняя ЗП по вакансиям"
+from employers e
+join vacancies v on e.employer_id = v.employer_id 
+join responces r on v.vacancy_id = r.vacancy_id
+join job_seekers js on r.seeker_id = js.seeker_id 
+join interviews i on r.responce_id = i.responce_id
+where i.result = 'Прошло'
+group by e.company_name;
